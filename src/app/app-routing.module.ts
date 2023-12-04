39f6f47592d18e574d61befa7bb943350bf53770
Artem Ivanov <ivanov@hostco.ru>
2023-12-04 15:33:19 +0500

    FormGroup, Validators, Login Component and Auth Service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		redirectTo: 'login',
+		pathMatch: 'full'
+	},
+	{
+		path: 'login',
+		component: LoginComponent
+	},
+	{
+		path: '**',
+		component: NotFoundComponent
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable, of, throwError } from 'rxjs';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class AuthService {
+
+	constructor(private router: Router) { }
+
+	setToken(token: string) {
+		localStorage.setItem('token', token);
+	}
+
+	getToken() {
+		return localStorage.getItem('token');
+	}
+
+	isLoggedIn() {
+		return this.getToken() !== null;
+	}
+
+	login(userInfo: { email: string, password: string }): Observable<string | boolean> {
+		if (userInfo.email === 'admin@mail.com' && userInfo.password === 'Admin123!') {
+			this.setToken('tokenValue');
+			return of(true);
+		}
+		return throwError(() => new Error('Failed to login'));
+	}
+}
